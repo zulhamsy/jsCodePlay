6e2a1cdf586350e6d68dd42e4b97a28eee390f84
zulhamsy <zulham.sy@gmail.com>
2020-05-13 11:07:50 +0700

    commit clock JS

---
+++ DOM/newClock/script.js
@@ -0,0 +1,22 @@
+let $ = (query) => document.querySelector(query);
+
+function printTime() {
+  let time = new Date();
+  $('.hour').textContent = addZero(time.getHours());
+  $('.minute').textContent = addZero(time.getMinutes());
+  if(time.getSeconds()%2 == 0) {
+    $('.second-tick').style.opacity = '0';
+  } else {
+    $('.second-tick').style.opacity = '1';
+  }
+  // $('.second-tick').textContent = time.getSeconds();
+}
+
+function addZero(num) {
+  if(num < 10) {
+    num = '0' + num.toString();
+  }
+  return num;
+}
+
+setInterval(printTime, 1000);
