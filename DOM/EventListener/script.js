e255b92aa763fa3b8657da88adedb971e3dc691c
zulhamsy <zulham.sy@gmail.com>
2020-05-18 08:25:37 +0700

    add add and remove button

--- DOM/EventListener/script.js
+++ DOM/EventListener/script.js
@@ -2,3 +2,22 @@ document.querySelector('.btn').addEventListener('click',
 function() {
     document.querySelector('#result').textContent = (Math.floor(Math.random()*1000));
 })
+
+// add card in card container
+document.querySelector('.add-card').addEventListener('click',
+    function() {
+        let newDiv = document.createElement('div');
+        let divLeng = document.querySelector('.card-container').children.length;
+        newDiv.appendChild(document.createTextNode(divLeng+1));
+        newDiv.className = 'card';
+        document.querySelector('.card-container').appendChild(newDiv);
+    }
+)
+
+// remove last element card
+document.querySelector('.remove-card').addEventListener('click', 
+    function() {
+        let parentNode = document.querySelector('.card-container');
+        parentNode.removeChild(parentNode.lastElementChild);
+    }
+)
