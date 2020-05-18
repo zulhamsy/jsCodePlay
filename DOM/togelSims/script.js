8f04d3bb7bef3c07771c0e3ed8e1e819d08e15e0
zulhamsy <zulham.sy@gmail.com>
2020-05-18 20:07:50 +0700

    add counter feature

--- DOM/togelSims/script.js
+++ DOM/togelSims/script.js
@@ -1,5 +1,7 @@
 const $ = (query) => document.querySelector(query);
 
+let counter = [0,0];
+
 function lock() {
   $('.choice').textContent = $('#num').value;
   if($('.choice').textContent) {
@@ -12,10 +14,14 @@ function play() {
   $('.generate').textContent = rand;
   if($('.choice').textContent == rand) {
     alert('Your choice is fucking right');
+    counter[1] ++;
+    $('.win').textContent = counter[1];
   } else {
     alert('You lose moron !!');
   }
   $('.lock').disabled = false;
+  counter[0] ++;
+  $('.attempt').textContent = counter[0];
 }
 
 $('.lock').addEventListener('click', lock);
