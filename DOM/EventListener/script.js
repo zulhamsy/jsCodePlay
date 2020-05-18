70f3cacf3aa1928257bb068520e9300fc2efab19
zulhamsy <zulham.sy@gmail.com>
2020-05-18 08:28:10 +0700

    assign querySelector to variable

--- DOM/EventListener/script.js
+++ DOM/EventListener/script.js
@@ -1,23 +1,25 @@
-document.querySelector('.btn').addEventListener('click',
+let $ = (query) => document.querySelector(query);
+
+$('.btn').addEventListener('click',
 function() {
-    document.querySelector('#result').textContent = (Math.floor(Math.random()*1000));
+    $('#result').textContent = (Math.floor(Math.random()*1000));
 })
 
 // add card in card container
-document.querySelector('.add-card').addEventListener('click',
+$('.add-card').addEventListener('click',
     function() {
         let newDiv = document.createElement('div');
-        let divLeng = document.querySelector('.card-container').children.length;
+        let divLeng = $('.card-container').children.length;
         newDiv.appendChild(document.createTextNode(divLeng+1));
         newDiv.className = 'card';
-        document.querySelector('.card-container').appendChild(newDiv);
+        $('.card-container').appendChild(newDiv);
     }
 )
 
 // remove last element card
-document.querySelector('.remove-card').addEventListener('click', 
+$('.remove-card').addEventListener('click', 
     function() {
-        let parentNode = document.querySelector('.card-container');
+        let parentNode = $('.card-container');
         parentNode.removeChild(parentNode.lastElementChild);
     }
 )
