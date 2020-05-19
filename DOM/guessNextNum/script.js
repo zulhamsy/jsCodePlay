07283099ed4c96c95047cd92ccf6e09a0635ff3f
zulhamsy <zulham.sy@gmail.com>
2020-05-19 20:59:54 +0700

    high or low alpha version

---
+++ DOM/guessNextNum/script.js
@@ -0,0 +1,41 @@
+const $ =(x) => document.querySelector(x);
+const $$ =(x) => document.querySelectorAll(x);
+
+const operate = {
+  more: (x,y) => x > y,
+  less: (x,y) => x < y,
+  equal: (x,y) => x == y
+};
+
+//let rand = Math.floor(Math.random()*10);
+
+function clear() {
+  $('.next').textContent = '';
+  $('.generated').textContent = Math.floor(Math.random()*10);
+  
+}
+
+function play() {
+  $('.next').textContent = Math.floor(Math.random()*10);
+  
+  let exec = $$('[name=choice]');
+  let x = $('.next').textContent;
+  let y = $('.generated').textContent;
+  
+  for (let i = 0; i < exec.length; i++) {
+    if (exec[i].checked) {
+      exec = exec[i].value;
+    }
+  }
+  
+  if(operate[exec](x,y)) {
+    alert('Your choice was right');
+  } else {
+    alert('You lose moron');
+  }
+  
+  setTimeout(clear, 1000);
+}
+
+$('.generated').textContent = Math.floor(Math.random()*10);
+$('.btn').addEventListener('click', play);
