0e96827ebdca5c8292ec630ad54fc5ba42a66c58
zulhamsy <zulham.sy@gmail.com>
2020-05-21 08:20:25 +0700

    add event delegation

--- DOM/EventListener/script.js
+++ DOM/EventListener/script.js
@@ -23,3 +23,24 @@ $('.remove-card').addEventListener('click',
         parentNode.removeChild(parentNode.lastElementChild);
     }
 )
+
+// change card color onclick
+
+// listener dibawah ini hanya akan diterapkan pada div.card yang tersedia pada HTML sebelum page load, jika ada .card baru maka listener tidak akan berfungsi pada element tsb
+/*$('.card').addEventListener('click',
+  function() {
+    $('.card').style.background = "black";
+    $('.card').style.color = "white";
+  }
+)*/
+
+// untuk fix tsb maka perlu event delegation yang memanfaatkan event bubbling
+$('.card-container').addEventListener('click',
+  function(event) {
+    if(event.target.classList.contains('card')) {
+      event.target.style.background = "black";
+      event.target.style.color = "white";
+      }
+      //console.log(event.target.classList.contains("card"));
+    }
+)
