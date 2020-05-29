5d2bed3a190001241710c8bf44430c29089b403b
zulhamsy <zulham.sy@gmail.com>
2020-05-29 14:42:34 +0700

    add spinner and result hider

--- project-loanCalculator/js/script.js
+++ project-loanCalculator/js/script.js
@@ -2,16 +2,27 @@ const $ = (x) => document.querySelector(x);
 
 $('#calc').addEventListener('click',
   () => {
-    let amount = Number($('#amount').value),
-        ipm = Number($('#interest').value) / 100 / 12,
-        month = Number($('#duration').value),
-        totalInt = Number((amount * ipm * month)),
-        totalPay = amount + totalInt,
-        monthPay = totalPay / 12;
-        
-    
-    
-    $('#result-total').textContent = `Rp.${totalPay.toFixed(2)}`;
-    $('#result-interest').textContent = `Rp.${monthPay.toFixed(2)}`
+    // load spinner
+    $('.hider').style.display = 'inline-block';
+    // run calc function after 1s
+    setTimeout(calc,1000);
   }
 );
+
+function calc() {
+    let amount = Number($('#amount').value),
+      ipm = Number($('#interest').value) / 100 / 12,
+      month = Number($('#duration').value),
+      totalInt = Number((amount * ipm * month)),
+      totalPay = amount + totalInt,
+      monthPay = totalPay / 12;
+
+
+
+    $('#result-total').textContent = `Rp.${totalPay.toFixed(2)}`;
+    $('#result-interest').textContent = `Rp.${monthPay.toFixed(2)}`;
+    // hide spinner
+    $('.hider').style.display = 'none';
+    // show result
+    $('.result').style.display = 'block';
+}
