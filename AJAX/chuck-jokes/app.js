75a426178215e5a8b4029fab03a8ef5d32e6a712
zulhamsy <zulham.sy@gmail.com>
2020-06-09 17:15:53 +0700

    finish chuck norris logic

--- AJAX/chuck-jokes/app.js
+++ AJAX/chuck-jokes/app.js
@@ -4,10 +4,24 @@ const $ = (x) => document.querySelector(x);
 $('form').addEventListener('submit',
     (e) => {
         let num = $('#numjokes').value;
-        // const xhr = new XMLHttpRequest;
-        console.log(num);
+        const xhr = new XMLHttpRequest;
         // open request
-        // xhr.open('GET', 'http://api.icndb.com/jokes/random/')
+        xhr.open('GET', `http://api.icndb.com/jokes/random/${num}`, true);
+        // onload
+        xhr.onload = function() {
+          if(this.status == 200) {
+            const jokes = JSON.parse(this.responseText);
+            let result = '';
+            jokes['value'].forEach((sjoke) => {
+              result += `
+                <li>${sjoke.joke}</li>
+              `;
+            });
+            $('ol').innerHTML = result;
+          }
+        }
+        // sent
+        xhr.send();
 
         e.preventDefault()
     }
