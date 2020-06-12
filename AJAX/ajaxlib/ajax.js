c81ef0888dab66762da3768c19ee694b3d57b357
zulhamsy <zulham.sy@gmail.com>
2020-06-12 23:06:51 +0700

    add post method

--- AJAX/ajaxlib/ajax.js
+++ AJAX/ajaxlib/ajax.js
@@ -17,6 +17,24 @@ class EasyHTTP extends XMLHttpRequest {
         this.send();
     }
     // post request
+    post(url, data, callback) {
+        this.open('POST', url, true);
+
+        this.setRequestHeader('Content-type', 'application/json');
+
+        this.onload = function () {
+            if (this.status == 200) {
+                callback(this.responseText);
+            } else {
+                callback({
+                    code: this.status,
+                    status: this.statusText
+                });
+            }
+        };
+
+        this.send(JSON.stringify(data));
+    }
     // put request
     // delete request
 }
--- AJAX/ajaxlib/app.js
+++ AJAX/ajaxlib/app.js
@@ -4,8 +4,28 @@ const http = new EasyHTTP;
 
 // console.log(result); // it just doesn't work, karena result di print sebelum nilainya direturn / ingat ASYNCHRONOUS process.
 
-http.get('https://jsonplaceholder.typicode.com/posts',
-    function (posts) {
-        console.log(posts);
+// GET all posts
+// http.get('https://jsonplaceholder.typicode.com/posts',
+//     function (posts) {
+//         console.log(posts);
+//     }
+// )
+
+// GET single post (by post id) atau bisa lainnya tergantung routes dari backend
+// http.get('https://jsonplaceholder.typicode.com/posts/1',
+//     function(post) {
+//         console.log(post); 
+//     }
+// )
+
+// POST request
+const data = {
+    title: "New post",
+    body: "Hi there"
+};
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, 
+    function(post) {
+        console.log(post);
     }
 )
