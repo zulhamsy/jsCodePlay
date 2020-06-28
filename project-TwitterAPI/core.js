2f27b834b0f8b46148c732f6426ba67cafa41c70
zulhamsy <zulham.sy@gmail.com>
2020-06-28 20:55:29 +0700

    finishing UI for Profile

--- project-TwitterAPI/core.js
+++ project-TwitterAPI/core.js
@@ -11,12 +11,11 @@ class HttpRequest {
 class UI {
   static displayProfile(input) {
     $('.result').className = 'result mt-4 show';
-    
-    // $('#img_profile').src = input.avatar_url;
-    console.log(input.login);
-    $('#name_profile').innerHTML = input.login;
-    $('#friends_profile').innerHTML = input.following;
-    $('#follower_profile').innerHTML = input.follower;
-    $('#repo_profile').innerHTML = input.public_repo;
+    console.log(input);
+    $('#img-profile').src = input.avatar_url;
+    $('#name-profile').textContent = `@${input.login}`;
+    $('#friend-profile').textContent = `${input.following} Following`;
+    $('#follower-profile').textContent = `${input.followers} Follower`;
+    $('#repo-profile').textContent = `${input.public_repos} Public Repo`;
   }
 }
