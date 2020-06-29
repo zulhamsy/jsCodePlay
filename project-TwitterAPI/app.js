f2fd1eecdb58189749ee3a0b3ec29329e33ce45f
Zulham Syafrawi <official.zulham@gmail.com>
2020-06-30 06:27:52 +0700

    add latest repo

--- project-TwitterAPI/app.js
+++ project-TwitterAPI/app.js
@@ -1,15 +1,17 @@
 const http = new HttpRequest;
 
-$('button[type="submit"').addEventListener('click', async function(e) {
+$('button[type="submit"').addEventListener('click', async function (e) {
   e.preventDefault();
 
   const text = $('input').value;
 
   if (text) {
     // fetch data from github
-    const profile = await http.getData(text);
+    const data = await http.getData(text);
     // display data to UI
-    UI.displayProfile(profile);
+    UI.displayProfile(data.profile);
+    // display repo to UI
+    UI.displayRepo(data.repo);
   } else {
     // hide result
     UI.resetDisplay();
--- project-TwitterAPI/core.js
+++ project-TwitterAPI/core.js
@@ -2,23 +2,40 @@ const $ = (q) => document.querySelector(q);
 
 class HttpRequest {
   async getData(input) {
-    let response = await fetch(`https://api.github.com/users/${input}`);
-    let data = await response.json();
-    return data;
+    let responseProfile = await fetch(`https://api.github.com/users/${input}`);
+    let resoponseRepo = await fetch(`https://api.github.com/users/${input}/repos?per_page=5&sort=updated&direction=desc`);
+    let profile = await responseProfile.json();
+    let repo = await resoponseRepo.json();
+    return {
+      profile,
+      repo
+    };
   }
 }
 
 class UI {
   static displayProfile(input) {
     $('.result').className = 'result mt-4 show';
-    
+
     $('#img-profile').src = input.avatar_url;
     $('#name-profile').textContent = `@${input.login}`;
     $('#friend-profile').textContent = `${input.following} Following`;
     $('#follower-profile').textContent = `${input.followers} Follower`;
     $('#repo-profile').textContent = `${input.public_repos} Public Repo`;
   }
-  
+
+  static displayRepo(input) {
+    let tableData = '';
+    input.forEach(repo => {
+      tableData += `
+        <tr>
+          <td>${repo.name}</td>
+        </tr>
+      `
+    });
+    $('.table').innerHTML = tableData;
+  }
+
   static resetDisplay() {
     $('.result').className = 'result mt-4 hidden';
   }
