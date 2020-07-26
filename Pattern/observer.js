9b378d326e65f077ce1facb7387eb47d182ae635
zulhamsy <zulham.sy@gmail.com>
2020-07-26 20:26:12 +0700

    basic Observer Pattern Design

---
+++ Pattern/observer.js
@@ -0,0 +1,34 @@
+class Subject {
+  constructor() {
+    this.observer = [];
+  }
+
+  subscribe(fn) {
+    this.observer.push(fn);
+  }
+
+  unsubscribe(fnRemove) {
+    this.observer = this.observer.filter(
+      fn => {
+        if (fn != fnRemove) return fn;
+      });
+  }
+
+  notify() {
+    this.observer.forEach(fn => {
+      fn();
+    });
+  }
+}
+
+const subs1 = function() {
+    console.log('First subs');
+  },
+  subs2 = function() {
+    console.log('Second subs');
+  };
+
+
+const subject = new Subject();
+subject.subscribe(subs1);
+subject.notify();
